8de84c840fdd39f44106b600f50155e872327eba
JhonAriza <arizaduarte24@gmail.com>
2023-04-10 17:51:40 -0500

    version 1

---
+++ database/factories/ArticuloFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Articulo>
+ */
+class ArticuloFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'nombre' => $this->faker->name,
+            'descripcion' => $this->faker->paragraph(1,5),
+            'stock' => $this->faker->numberBetween(1,999),
+ 
+        ];
+    }
+}
