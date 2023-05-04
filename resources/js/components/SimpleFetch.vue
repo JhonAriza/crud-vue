c90c49657cb06cfc015be097b86fb570de273300
JhonAriza <arizaduarte24@gmail.com>
2023-05-04 15:39:25 -0500

    se ajusta vista de usuarios de api

--- resources/js/components/SimpleFetch.vue
+++ resources/js/components/SimpleFetch.vue
@@ -1,10 +1,33 @@
 <template>
-  <h2>Simple Fetch</h2>
-  <ul v-if="users">
-    <li v-for="user in users" :key="user.id">
-      {{ user.name }} - {{ user.email }}
-    </li>
-  </ul>
+  <div class="contenedor">
+  <h2>Simple Fetch Usuarios</h2><p>people Users</p>
+  <table  class="table border">
+        <thead  >
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">Nombre</th>
+            <th scope="col">email</th>
+            <th scope="col">city</th>
+            <th scope="col">city.street</th>
+            <th scope="col">city.lat</th>
+            <th scope="col">city.lng</th>
+ 
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="usuario in users" :key="usuario.id">
+            <th scope="row">{{ usuario.id }}</th>
+            <td>{{ usuario.name }}</td>
+            <td>{{ usuario.email }}</td>
+            <td>{{ usuario.address.city }}</td>
+            <td>{{ usuario.address.street}}</td>
+
+            <td>{{ usuario.address.geo.lat }}</td>
+            <td>{{ usuario.address.geo.lng }}</td>
+
+          </tr>
+        </tbody>
+      </table></div>
 </template>
 
 <script>
